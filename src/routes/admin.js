a7fbd0a16bdc4daa9e3ca3cba82c6600b306d35f
PhanTienHuy <huyphan0220@gmail.com>
2024-01-18 22:50:21 +0700

    up1

---
+++ src/routes/admin.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const OrderController = require("../controllers/admin/OrderController");
+const ProductController = require("../controllers/admin/ProductController");
+const AuthController = require("../controllers/admin/AuthController");
+const middleware = require("../middleware/middleware");
+
+router.get(
+  "/dashboard",
+  middleware.middlewareAdmin,
+  OrderController.OrderDashBoard
+);
+router.get("/orders", middleware.middlewareAdmin, OrderController.orderIndex);
+router.get(
+  "/orders/:order_id",
+  middleware.middlewareAdmin,
+  OrderController.getOrderDetail
+);
+router.put(
+  "/orders/confirm/:order_id",
+  middleware.middlewareAdmin,
+  OrderController.confirmOrder
+);
+router.delete(
+  "/orders/delete/:order_id",
+  middleware.middlewareAdmin,
+  OrderController.deleteOrder
+);
+
+router.get(
+  "/products",
+  middleware.middlewareAdmin,
+  ProductController.productIndex
+);
+router.post(
+  "/products/store",
+  middleware.middlewareAdmin,
+  ProductController.storeProduct
+);
+router.delete("/products/delete/:product_id", ProductController.deleteProduct);
+router.put("/products/update/", ProductController.updateProduct);
+router.get("/categories", ProductController.getCategoryAdd);
+
+router.get(
+  "/authentication",
+  middleware.middlewareAdmin,
+  AuthController.handleAuthAdmin
+);
+router.get("/logout", middleware.middlewareAdmin, AuthController.logoutAdmin);
+router.post("/login", AuthController.handleLoginAdmin);
+
+module.exports = router;
